da519cabf2c9c23c9b9cd8defd7e796a5800bd55
Maciej Piszczek <maciej.piszczek93@gmail.com>
2023-02-21 10:52:50 +0100

    added tests for Size and Tier models

---
+++ backend/tiers/tests/test_models_tier.py
@@ -0,0 +1,18 @@
+import pytest
+
+from tiers.models import Tier
+
+
+@pytest.mark.models
+def test_tier_creation(db, create_test_tier, create_test_size):
+    assert isinstance(create_test_tier, Tier)
+    assert create_test_tier.name == 'test tier'
+    assert not create_test_tier.original_link
+    assert create_test_tier.expiring_links
+    assert create_test_tier.size.get(height=200) == create_test_size
+    assert create_test_tier.__str__() == 'test tier'
+
+
+@pytest.mark.models
+def test_tier_fields(db, create_test_tier):
+    assert [*create_test_tier.__dict__] == ['_state', 'id', 'name', 'original_link', 'expiring_links']
