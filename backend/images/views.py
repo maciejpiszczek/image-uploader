fdc158c660a9d5693368372e829efaeaf7b1b55c
Maciej Piszczek <maciej.piszczek93@gmail.com>
2023-02-21 13:33:17 +0100

    added serializers and migration for images and thumbnails, router for images app

--- backend/images/views.py
+++ backend/images/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from images.models import Image
+from images.serializers import ImageSerializer, ImageSerializerWithoutOriginalLink
+
+
+class ImageViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Image.objects.filter(author=user)
+
+    def get_serializer_class(self):
+        user = self.request.user
+        if user.tier.original_link:
+            return ImageSerializer
+        else:
+            return ImageSerializerWithoutOriginalLink
