54e43fc791e22beb6e3e4387e8117a697135488a
Maciej Piszczek <maciej.piszczek93@gmail.com>
2023-02-26 19:18:07 +0100

    added 'tier' field to custom user admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,12 +1,31 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from . import models
 
 
 class CustomUserAdmin(UserAdmin):
     model = get_user_model()
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "tier",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
 
 
 admin.site.register(models.CustomUser, CustomUserAdmin)
